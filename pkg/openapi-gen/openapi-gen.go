8d1383e6768fd2d3d6117f11bda78048305bccd4
Sean Condon <sean@opennetworking.org>
2020-08-28 14:52:21 +0100

    openapi-gen: Added "target" parameter to paths (#45)

--- pkg/openapi-gen/openapi-gen.go
+++ pkg/openapi-gen/openapi-gen.go
@@ -28,19 +28,27 @@ import (
 var swagger openapi3.Swagger
 
 var respGet200Desc = "GET OK 200"
+var pathPrefix string
+var targetParameter *openapi3.ParameterRef
 
-func BuildOpenapi(yangSchema *ytypes.Schema) (*openapi3.Swagger, error) {
+func BuildOpenapi(yangSchema *ytypes.Schema, modelType string, modelVersion string) (*openapi3.Swagger, error) {
+	pathPrefix = fmt.Sprintf("/%s/v%s/{target}", strings.ToLower(modelType), modelVersion)
+	targetParameter = targetParam()
+
 	topEntry := yangSchema.SchemaTree["Device"]
-	paths, components, err := buildSchema(topEntry, yang.TSFalse, "")
+	paths, components, err := buildSchema(topEntry, yang.TSFalse, pathPrefix)
 	if err != nil {
 		return nil, err
 	}
 
+	components.Parameters = make(map[string]*openapi3.ParameterRef)
+	components.Parameters["target"] = targetParameter
+
 	swagger = openapi3.Swagger{
 		OpenAPI: "3.0.0",
 		Info: &openapi3.Info{
-			Title:   "onos-config model plugin",
-			Version: "1.0.0-oas3",
+			Title:   fmt.Sprintf("%s onos-config model plugin", modelType),
+			Version: modelVersion,
 		},
 		Paths:      paths,
 		Components: *components,
@@ -58,6 +66,29 @@ func BuildOpenapi(yangSchema *ytypes.Schema) (*openapi3.Swagger, error) {
 	return &swagger, nil
 }
 
+func targetParam() *openapi3.ParameterRef {
+
+	stringContent := openapi3.NewContent()
+	mt := openapi3.NewMediaType()
+	mt.Schema = &openapi3.SchemaRef{
+		Value: openapi3.NewStringSchema(),
+	}
+	stringContent["text/plain; charset=utf-8"] = mt
+
+	targetParam := openapi3.ParameterRef{
+		Value: &openapi3.Parameter{
+			Name:        "target",
+			In:          "path",
+			Description: "target (device in onos-config)",
+			Required:    true,
+			Example:     "internal",
+			Content:     stringContent,
+		},
+	}
+
+	return &targetParam
+}
+
 // buildSchema is a recursive function to extract a list of read only paths from a YGOT schema
 func buildSchema(deviceEntry *yang.Entry, parentState yang.TriState, parentPath string) (openapi3.Paths, *openapi3.Components, error) {
 	openapiPaths := make(openapi3.Paths)
@@ -65,7 +96,7 @@ func buildSchema(deviceEntry *yang.Entry, parentState yang.TriState, parentPath 
 		Schemas: make(map[string]*openapi3.SchemaRef),
 	}
 	for _, dirEntry := range deviceEntry.Dir {
-		itemPath := formatName(dirEntry, false, parentPath, "")
+		itemPath := formatName(dirEntry, false, parentPath)
 		if dirEntry.IsLeaf() || dirEntry.IsLeafList() {
 			// No need to recurse
 			var schemaVal *openapi3.Schema
@@ -219,12 +250,8 @@ func buildSchema(deviceEntry *yang.Entry, parentState yang.TriState, parentPath 
 	return openapiPaths, &openapiComponents, nil
 }
 
-func formatName(dirEntry *yang.Entry, isList bool, parentPath string, subpathPrefix string) string {
+func formatName(dirEntry *yang.Entry, isList bool, parentPath string) string {
 	parentAndSubPath := parentPath
-	if subpathPrefix != "/" {
-		parentAndSubPath = fmt.Sprintf("%s%s", parentPath, subpathPrefix)
-	}
-
 	var name string
 	if isList {
 		//have to ensure index order is consistent where there's more than one
@@ -245,7 +272,7 @@ func newPathItem(dirEntry *yang.Entry, itemPath string, parentPath string) *open
 	getOp.Summary = fmt.Sprintf("GET %s Generated from YANG model", itemPath)
 	getOp.OperationID = fmt.Sprintf("get%s", toCamelCase(itemPath))
 	getOp.Tags = []string{toCamelCase(parentPath)}
-	getOp.Responses = openapi3.NewResponses()
+	getOp.Responses = make(openapi3.Responses)
 
 	parameters := make(openapi3.Parameters, 0)
 	if dirEntry.IsList() {
@@ -265,6 +292,11 @@ func newPathItem(dirEntry *yang.Entry, itemPath string, parentPath string) *open
 			parameters = append(parameters, &p)
 		}
 	}
+	targetParameterRef := openapi3.ParameterRef{
+		Ref:   fmt.Sprintf("#/components/parameters/target"),
+		Value: targetParameter.Value,
+	}
+	parameters = append(parameters, &targetParameterRef)
 
 	newPath := openapi3.PathItem{
 		Get:         getOp,
