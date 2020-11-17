37cd4361f05634cfad723c96b49201f685397422
sbconsulting <sbconsulting@users.noreply.github.com>
2020-11-16 23:40:44 -0800

    Make modeldata easily consumable as a library import (#56)
    * make modeldata easily consumable by import
    
    * release 0.6.15
    
    Co-authored-by: Scott Baker <smbaker@gmail.com>

--- modelplugin/aether-1.0.0/go.mod
+++ modelplugin/aether-1.0.0/go.mod
@@ -9,8 +9,6 @@ require (
 	github.com/openconfig/gnmi v0.0.0-20190823184014-89b2bf29312c
 	github.com/openconfig/goyang v0.0.0-20200115183954-d0a48929f0ea
 	github.com/openconfig/ygot v0.6.1-0.20200103195725-e3c44fa43926
-	golang.org/x/lint v0.0.0-20190313153728-d0100b6bd8b3 // indirect
-	honnef.co/go/tools v0.0.0-20190523083050-ea95bdfd59fc // indirect
 )
 
 replace github.com/onosproject/config-models => ../../
--- modelplugin/aether-1.0.0/modelmain.go
+++ modelplugin/aether-1.0.0/modelmain.go
@@ -16,67 +16,10 @@
 package main
 
 import (
-	"fmt"
-	_ "github.com/golang/protobuf/proto"
-	"github.com/onosproject/config-models/modelplugin/aether-1.0.0/aether_1_0_0"
-	"github.com/openconfig/gnmi/proto/gnmi"
-	"github.com/openconfig/goyang/pkg/yang"
-	_ "github.com/openconfig/ygot/genutil"
-	_ "github.com/openconfig/ygot/ygen"
-	"github.com/openconfig/ygot/ygot"
-	_ "github.com/openconfig/ygot/ytypes"
+	"github.com/onosproject/config-models/modelplugin/aether-1.0.0/modelplugin"
 )
 
 //go:generate go run github.com/openconfig/ygot/generator -path=yang -output_file=aether_1_0_0/generated.go -package_name=aether_1_0_0 -generate_fakeroot aether-subscriber@2020-08-18.yang apn-profile@2020-08-18.yang up-profile@2020-08-18.yang qos-profile@2020-08-18.yang access-profile@2020-08-18.yang
 
-type modelplugin string
-
-const modeltype = "Aether"
-const modelversion = "1.0.0"
-const modulename = "aether.so.1.0.0"
-
-var modelData = []*gnmi.ModelData{
-	{Name: "aether-subscriber", Organization: "Open Networking Foundation", Version: "2020-08-18"},
-	{Name: "apn-profile", Organization: "Open Networking Foundation", Version: "2020-08-18"},
-	{Name: "up-profile", Organization: "Open Networking Foundation", Version: "2020-08-18"},
-	{Name: "qos-profile", Organization: "Open Networking Foundation", Version: "2020-08-18"},
-	{Name: "access-profile", Organization: "Open Networking Foundation", Version: "2020-08-18"},
-}
-
-func (m modelplugin) ModelData() (string, string, []*gnmi.ModelData, string) {
-	return modeltype, modelversion, modelData, modulename
-}
-
-// UnmarshallConfigValues allows Device to implement the Unmarshaller interface
-func (m modelplugin) UnmarshalConfigValues(jsonTree []byte) (*ygot.ValidatedGoStruct, error) {
-	device := &aether_1_0_0.Device{}
-	vgs := ygot.ValidatedGoStruct(device)
-
-	if err := aether_1_0_0.Unmarshal([]byte(jsonTree), device); err != nil {
-		return nil, err
-	}
-
-	return &vgs, nil
-}
-
-func (m modelplugin) Validate(ygotModel *ygot.ValidatedGoStruct, opts ...ygot.ValidationOption) error {
-	deviceDeref := *ygotModel
-	device, ok := deviceDeref.(*aether_1_0_0.Device)
-	if !ok {
-		return fmt.Errorf("unable to convert model in to aether_1_0_0")
-	}
-	return device.Validate()
-}
-
-func (m modelplugin) Schema() (map[string]*yang.Entry, error) {
-	return aether_1_0_0.UnzipSchema()
-}
-
-// GetStateMode returns an int - we do not use the enum because we do not want a
-// direct dependency on onos-config code (for build optimization)
-func (m modelplugin) GetStateMode() int {
-	return 0 // modelregistry.GetStateNone
-}
-
 // ModelPlugin is the exported symbol that gives an entry point to this shared module
-var ModelPlugin modelplugin
+var ModelPlugin modelplugin.ModelPlugin
