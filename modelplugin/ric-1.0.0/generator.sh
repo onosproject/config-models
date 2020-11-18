1c1079efefaf2d96dca86423f8dfeaa7308300e2
adibrastegarnia <adib.rastegarnia@gmail.com>
2020-11-18 12:39:13 -0800

    Use a test yang model

--- modelplugin/ric-1.0.0/generator.sh
+++ modelplugin/ric-1.0.0/generator.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-go run github.com/openconfig/ygot/generator -path=yang -output_file=ric_1_0_0/generated.go -package_name=ric_1_0_0 -generate_fakeroot xapp@2020-11-18.yang \
+go run github.com/openconfig/ygot/generator -path=yang -output_file=ric_1_0_0/generated.go -package_name=ric_1_0_0 -generate_fakeroot test1@2020-11-18.yang \
 
 
 sedi=(-i)
