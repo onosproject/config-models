3aab7647a145f3cce7a9668e9b82e7e98b519727
sbconsulting <sbconsulting@users.noreply.github.com>
2020-11-19 08:36:26 -0800

    Initial draft of Enterprise modeling (#59)
    * start work on enterprise models
    
    * rename enterprise-subscriber to device-range
    
    * add enterprise, site, and use-case models
    
    * add application, deployment-environment, qos, and visibility
    
    Co-authored-by: Scott Baker <smbaker@gmail.com>

---
+++ modelplugin/enterprise-1.0.0/go.mod
@@ -0,0 +1,16 @@
+module github.com/onosproject/config-models/modelplugin/enterprise-1.0.0
+
+go 1.14
+
+require (
+	github.com/ghodss/yaml v1.0.0
+	github.com/golang/protobuf v1.3.3
+	github.com/onosproject/config-models v0.6.9
+	github.com/openconfig/gnmi v0.0.0-20190823184014-89b2bf29312c
+	github.com/openconfig/goyang v0.0.0-20200115183954-d0a48929f0ea
+	github.com/openconfig/ygot v0.6.1-0.20200103195725-e3c44fa43926
+	golang.org/x/lint v0.0.0-20190313153728-d0100b6bd8b3 // indirect
+	honnef.co/go/tools v0.0.0-20190523083050-ea95bdfd59fc // indirect
+)
+
+replace github.com/onosproject/config-models => ../../
---
+++ modelplugin/enterprise-1.0.0/modelmain.go
@@ -0,0 +1,25 @@
+// Copyright 2019-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// A plugin for the YGOT model of enterprise-1.0.0.
+package main
+
+import (
+	"github.com/onosproject/config-models/modelplugin/enterprise-1.0.0/modelplugin"
+)
+
+//go:generate go run github.com/openconfig/ygot/generator -path=yang -output_file=enterprise_1_0_0/generated.go -package_name=enterprise_1_0_0 -generate_fakeroot device-range@2020-11-18.yang device-group@2020-11-18.yang enterprise@2020-11-18.yang site@2020-11-18.yang use-case@2020-11-18.yang deployment-environment@2020-11-18.yang application@2020-11-18.yang visibility@2020-11-18.yang qos@2020-11-18.yang
+
+// ModelPlugin is the exported symbol that gives an entry point to this shared module
+var ModelPlugin modelplugin.ModelPlugin
