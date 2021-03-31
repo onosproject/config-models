a89dad0afe0665fa651c653ea9eed83a9a63762b
Sean Condon <sean@opennetworking.org>
2021-03-31 18:05:24 +0100

    Further tidy up of config models. Updates for TestDevice-1.0.0 new yang (#85)

--- modelplugin/stratum-1.0.0/go.mod
+++
@@ -1,9 +0,0 @@
-module github.com/onosproject/config-models/modelplugin/stratum-1.0.0
-
-go 1.14
-
-require (
-	github.com/openconfig/gnmi v0.0.0-20200508230933-d19cebf5e7be
-	github.com/openconfig/goyang v0.2.1
-	github.com/openconfig/ygot v0.8.12
-)
---
+++ modelplugin/testdevice-1.0.0/testdevice_1_0_0/generated_test.go
@@ -0,0 +1,56 @@
+// Copyright 2021-present Open Networking Foundation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package testdevice_1_0_0
+
+import (
+	"gotest.tools/assert"
+	"testing"
+)
+
+func Test_generated(t *testing.T) {
+	assert.Equal(t, 9, len(SchemaTree))
+
+	for k, v := range SchemaTree {
+		switch k {
+		case "Device":
+			assert.Equal(t, "", v.Description)
+		case "Test1_Cont1A":
+			assert.Equal(t, "", v.Description)
+			//assert.Equal(t, "The top level container", v.Description) // Not until ygot 10.4
+		case "Test1_Cont1A_Cont2A":
+			assert.Equal(t, "", v.Description)
+			//assert.Equal(t, "The 2nd level container", v.Description) // Not until ygot 10.4
+		case "Test1_Cont1A_List2A":
+			assert.Equal(t, "", v.Description)
+			//assert.Equal(t, "A simple list of configuration items", v.Description) // Not until ygot 10.4
+		case "Test1_Cont1BState":
+			assert.Equal(t, "", v.Description)
+			//assert.Equal(t, "A second top level container - this one for state attributes. Edit symbol should not be visible", v.Description) // Not until ygot 10.4
+		case "Test1_Cont1BState_List2B":
+			assert.Equal(t, "", v.Description)
+			//assert.Equal(t, "A simple list of state items", v.Description) // Not until ygot 10.4
+		case "Test1_Cont1A_List4":
+			assert.Equal(t, "", v.Description)
+			//assert.Equal(t, "A simple list of state items", v.Description) // Not until ygot 10.4
+		case "Test1_Cont1A_List5":
+			assert.Equal(t, "", v.Description)
+			//assert.Equal(t, "A simple list of state items", v.Description) // Not until ygot 10.4
+		case "Test1_Cont1A_List4_List4A":
+			assert.Equal(t, "", v.Description)
+			//assert.Equal(t, "A simple list of state items", v.Description) // Not until ygot 10.4
+		default:
+			t.Errorf("unexpected schema entry %s", k)
+		}
+	}
+}
