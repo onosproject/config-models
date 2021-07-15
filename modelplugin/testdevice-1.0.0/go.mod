cc88d5f8da69073c36584e51dfd44cbda0720186
Sean Condon <sean@opennetworking.org>
2021-07-15 10:39:24 +0100

    Handle 'required' in containers properly and tidy up

--- modelplugin/testdevice-1.0.0/go.mod
+++ modelplugin/testdevice-1.0.0/go.mod
@@ -3,8 +3,8 @@ module github.com/onosproject/config-models/modelplugin/testdevice-1.0.0
 go 1.14
 
 require (
-	github.com/openconfig/goyang v0.2.2
-	github.com/openconfig/ygot v0.10.5
+	github.com/openconfig/goyang v0.2.5
+	github.com/openconfig/ygot v0.11.2
 	github.com/pkg/errors v0.9.1 // indirect
 	gotest.tools v2.2.0+incompatible
 )
--- pkg/openapi-gen/openapi-gen.go
+++ pkg/openapi-gen/openapi-gen.go
@@ -35,7 +35,6 @@ var targetParameter *openapi3.ParameterRef
 type ApiGenSettings struct {
 	ModelType    string
 	ModelVersion string
-	Example      string
 	Title        string
 }
 
@@ -51,16 +50,13 @@ func (settings ApiGenSettings) ApplyDefaults() {
 	if settings.Title == "" {
 		settings.Title = fmt.Sprintf("%s onos-config model plugin", settings.ModelType)
 	}
-	if settings.Example == "" {
-		settings.Example = "internal"
-	}
 }
 
 func BuildOpenapi(yangSchema *ytypes.Schema, settings ApiGenSettings) (*openapi3.Swagger, error) {
 	settings.ApplyDefaults()
 
 	pathPrefix = fmt.Sprintf("/%s/v%s/{target}", strings.ToLower(settings.ModelType), settings.ModelVersion)
-	targetParameter = targetParam(settings.Example)
+	targetParameter = targetParam()
 
 	topEntry := yangSchema.SchemaTree["Device"]
 	paths, components, err := buildSchema(topEntry, yang.TSFalse, "")
@@ -103,7 +99,7 @@ func BuildOpenapi(yangSchema *ytypes.Schema, settings ApiGenSettings) (*openapi3
 	return &swagger, nil
 }
 
-func targetParam(example string) *openapi3.ParameterRef {
+func targetParam() *openapi3.ParameterRef {
 
 	stringContent := openapi3.NewContent()
 	mt := openapi3.NewMediaType()
@@ -118,7 +114,6 @@ func targetParam(example string) *openapi3.ParameterRef {
 			In:          "path",
 			Description: "target (device in onos-config)",
 			Required:    true,
-			Example:     example,
 			Content:     stringContent,
 		},
 	}
@@ -328,7 +323,11 @@ func buildSchema(deviceEntry *yang.Entry, parentState yang.TriState, parentPath 
 						}
 					}
 					openapiComponents.Schemas[k] = v
-				case "string", "boolean", "integer": // leaf as a child of list
+				case "string", "boolean", "integer", "number": // leaf as a child of list
+					if v.Value.Required != nil {
+						schemaVal.Required = append(schemaVal.Required, v.Value.Required...)
+						v.Value.Required = nil
+					}
 					schemaVal.Properties[v.Value.Title] = v
 				case "leaf-list":
 					v.Value.Type = "array"
