264c17d25dddab42fa256ade7114d2f324411608
Sean Condon <sean@opennetworking.org>
2021-09-09 10:43:05 +0100

    TestDevice-1.0.0 updating for renamed YANG files

--- modelplugin/testdevice-1.0.0/generator.sh
+++ modelplugin/testdevice-1.0.0/generator.sh
@@ -2,7 +2,11 @@
 
 go run github.com/openconfig/ygot/generator -path=../../../onos-helm-charts/config-models/testdevice-1.x/files/yang \
 -output_file=testdevice_1_0_0/generated.go -package_name=testdevice_1_0_0 \
--generate_fakeroot -include_descriptions test1@2018-02-20.yang test1-extra@2021-04-01.yang
+-generate_fakeroot -include_descriptions onf-test1@2018-02-20.yang onf-test1-extra@2021-04-01.yang
+
+pyang --lint \
+../../../onos-helm-charts/config-models/testdevice-1.x/files/yang/onf-test1@2018-02-20.yang \
+../../../onos-helm-charts/config-models/testdevice-1.x/files/yang/onf-test1-extra@2021-04-01.yang
 
 sedi=(-i)
 case "$(uname)" in
--- modelplugin/testdevice-1.0.0/testdevice_1_0_0/generated_test.go
+++ modelplugin/testdevice-1.0.0/testdevice_1_0_0/generated_test.go
@@ -25,21 +25,21 @@ func Test_generated(t *testing.T) {
 		switch k {
 		case "Device":
 			assert.Equal(t, "", v.Description)
-		case "Test1_Cont1A":
+		case "OnfTest1_Cont1A":
 			assert.Equal(t, "The top level container", v.Description)
-		case "Test1_Cont1A_Cont2A":
+		case "OnfTest1_Cont1A_Cont2A":
 			assert.Equal(t, "The 2nd level container", v.Description)
-		case "Test1_Cont1A_List2A":
+		case "OnfTest1_Cont1A_List2A":
 			assert.Equal(t, "A simple list of configuration items", v.Description)
-		case "Test1_Cont1BState":
+		case "OnfTest1_Cont1BState":
 			assert.Equal(t, "A second top level container - this one for state attributes. Edit symbol should not be visible", v.Description)
-		case "Test1_Cont1BState_List2B":
+		case "OnfTest1_Cont1BState_List2B":
 			assert.Equal(t, "A simple list of state items", v.Description)
-		case "Test1_Cont1A_List4":
+		case "OnfTest1_Cont1A_List4":
 			assert.Equal(t, "A list with a leafref index", v.Description)
-		case "Test1_Cont1A_List5":
+		case "OnfTest1_Cont1A_List5":
 			assert.Equal(t, "A list with 2 keys", v.Description)
-		case "Test1_Cont1A_List4_List4A":
+		case "OnfTest1_Cont1A_List4_List4A":
 			assert.Equal(t, "A list within a list with 2 keys as leaf refs", v.Description)
 		default:
 			t.Errorf("unexpected schema entry %s", k)
