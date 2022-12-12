4a246c41e42ba3c757604088ace8487636d87903
Sean Condon <sean.condon@intel.com>
2022-12-12 22:00:16 +0000

    add a common yang base dir (#322)
    * add a common yang base dir
    
    * fix tests

--- pkg/compiler/metadata_test.go
+++ pkg/compiler/metadata_test.go
@@ -12,17 +12,19 @@ import (
 func TestLoadMetaData(t *testing.T) {
 	path := "../../test/metadata"
 
-	md := &MetaData{}
+	md := &MetaData{LintModel: true, RequireHyphenated: true}
 	if err := LoadMetaData(path, "valid", md); err != nil {
 		t.Fatal(err)
 	}
 
 	err := LoadMetaData(path, "not-existing", md)
 	assert.Error(t, err)
+	assert.True(t, md.LintModel)
+	assert.True(t, md.RequireHyphenated)
 }
 
 func TestValidateMetaData(t *testing.T) {
-	missingName := &MetaData{Name: ""}
+	missingName := &MetaData{Name: "", LintModel: true, RequireHyphenated: true}
 	err := ValidateMetaData(missingName)
 	assert.Error(t, err)
 	assert.Equal(t, "name is mandatory", err.Error())
